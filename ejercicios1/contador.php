f6bb67c4bd917135ed1f0d5841700cf79b1c9009
davidgallart <10724637@ieselgrao.org>
2024-09-23 18:19:59 +0200

    entrega

--- ejercicios1/contador.php
+++ ejercicios1/contador.php
@@ -9,7 +9,7 @@
 <body>
     <h1>Ejercicio 6</h1>
     <?php
-
+echo "<h2>Cuenta del 1 al 100</h2> <br>";
     for ($i = 0; $i < 101; $i++) {
 
         if ($i == 100) {
@@ -20,6 +20,8 @@
     }
     $n = 10;
     echo "<br>";
+    echo "<h2>Cuenta del 10 al 0</h2> <br>";
+
     while ($n >= 0) {
         if ($n == 0) {
             echo $n;
--- ejercicios1/ejercicio3.php
+++ ejercicios1/ejercicio3.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,21 +6,20 @@
     <title>Ejercicio 3</title>
 </head>
 <body>
-    
 <?php
  $num1 = 3;
  $num2 = 5;
  $num3 = 8;
  $num1 *= 4;
- echo $num1;
- echo $num1 <= $num2;
- echo $num3 > $num1 and $num3 > $num2;
- echo $num3 > $num1 or $num3 > $num2;
- echo $num1 > $num2 xor $num1 > $num3;
+ echo $num1 . "<br>";
+ echo $num1 <= $num2 . "<br>";
+ echo $num3 > $num1 and $num3 > $num2 . "<br>";
+ echo $num3 > $num1 or $num3 > $num2 . "<br>";
+ echo $num1 > $num2 xor $num1 > $num3 . "<br>";
  $num3--;
- echo $num3;
+ echo $num3 . "<br>";
  $num3 += $num1;
- echo $num3;
-?>
+ echo $num3 . "<br>";
+?> 
 </body>
 </html>
