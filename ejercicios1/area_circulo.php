8dcdaf490e787e783fabbe68b1b322725dbb3e26
davidgallart <10724637@ieselgrao.org>
2024-09-23 18:09:49 +0200

    hasta el 6

--- ejercicios1/area_circulo.php
+++ ejercicios1/area_circulo.php
@@ -4,16 +4,16 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Documento</title>
+    <title>Ejercicio 2</title>
 </head>
 <body>
     <h1>Pagina de bienvenida</h1>
     <?php
-    $radio = 3.5e2;
+    $radio = 3.5;
     define('PI',3.1416);
-    $area = PI * $radio;
+    $area = PI * sqrt($radio);
 
-    echo("El area del circulo es $area")
+    echo "El area del circulo es $area";
     ?>
 </body>
 </html>
